5bb10f905187b19f56d7b681c5696c320844b4e6
arsrsyh <gotrany97@gmail.com>
2023-08-04 11:03:45 +0800

    fix: auth action

--- src/redux/actions/auth.js
+++ src/redux/actions/auth.js
@@ -11,6 +11,7 @@ export const asyncLogin = createAsyncThunk(
       form.append('confirmPassword', payload.confirmPassword);
 
       const {data} = await http().post('/auth/login', form.toString());
+      console.log(form);
       return data.results.token;
     } catch (error) {
       const message = error?.response?.message;
@@ -27,13 +28,15 @@ export const asyncRegister = createAsyncThunk(
   'asyncRegister',
   async (payload, {rejectWithValue}) => {
     try {
-      const form = new URLSearchParams();
-      form.append('fullName', payload.fullName);
-      form.append('email', payload.email);
-      form.append('password', payload.password);
-      form.append('confirmPassword', payload.confirmPassword);
+      const form = new URLSearchParams({
+        fullName: payload.fullName,
+        email: payload.email,
+        password: payload.password,
+        confirmPassword: payload.confirmPassword,
+      }).toString();
 
-      const {data} = await http().post('/auth/register', form.toString());
+      const {data} = await http().post('/auth/register', form);
+
       return data.results.token;
     } catch (error) {
       const message = error?.response?.message;
