6356bd23ee3c347e8f63dceb78642c0f2b17ef5d
arsrsyh <gotrany97@gmail.com>
2023-08-12 15:11:40 +0800

    MyBooking

--- src/screens/MyBooking/index.jsx
+++ src/screens/MyBooking/index.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-native/no-inline-styles */
 import {
   View,
   Text,
@@ -5,14 +6,28 @@ import {
   StyleSheet,
   Dimensions,
 } from 'react-native';
-import React from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 import AntDesign from 'react-native-vector-icons/dist/AntDesign';
 import MaterialIcons from 'react-native-vector-icons/dist/MaterialIcons';
 import globalStyle from '../../assets/globalStyles';
 import moment from 'moment';
 import NoTicket from '../../components/NoTicket';
+import {useSelector} from 'react-redux';
+import http from '../../helper/http';
 
 export default function MyBooking() {
+  const [dataHistory, setDataHistory] = useState([]);
+  const token = useSelector(state => state.auth.token);
+  const getData = useCallback(async () => {
+    const {data} = await http(token).get('/history');
+
+    setDataHistory(data.results);
+  }, [token]);
+
+  useEffect(() => {
+    getData();
+  }, [getData]);
+
   return (
     <View>
       <View style={globalStyle.bookingHeader}>
@@ -26,20 +41,26 @@ export default function MyBooking() {
             <Text style={globalStyle.monthData}>{moment().format('MMMM')}</Text>
           </View>
         </View>
-        <View style={globalStyle.myBokingContaner}>
-          <View style={globalStyle.dateStyle}>
-            <Text style={globalStyle.date}>15</Text>
-            <Text>Wed</Text>
-          </View>
-          <View style={{gap: 10}}>
-            <Text style={globalStyle.fontData}>Sights & Sounds Exhibition</Text>
-            <Text>Jakarta, Indonesia</Text>
-            <Text>Wed, 15 Nov, 4:00 PM</Text>
-            <TouchableOpacity>
-              <Text>Detail</Text>
-            </TouchableOpacity>
-          </View>
-        </View>
+        {dataHistory.map(history => {
+          return (
+            <View style={globalStyle.myBokingContaner}>
+              <View style={globalStyle.dateStyle}>
+                <Text style={globalStyle.date}>15</Text>
+                <Text>Wed</Text>
+              </View>
+              <View style={{gap: 10}}>
+                <Text style={globalStyle.fontData}>{history.title}</Text>
+                <Text>{history.location}</Text>
+                <Text>
+                  {moment(history.createdAt).format('ddd, DD MMM YYYY, h:mm a')}
+                </Text>
+                <TouchableOpacity>
+                  <Text>detail</Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+          );
+        })}
         {/* <NoTicket /> */}
       </View>
     </View>
